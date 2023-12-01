4f0f18a701598727273e716a31154299b99e4011
DaviRSouza <144457305+DaviRSouza@users.noreply.github.com>
2023-12-01 01:42:10 -0300

    Star Bucks Page

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function trocaImg(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+function corCirculo(cor){
+    const circulo = document.querySelector('.circulo')
+    circulo.style.backgroundColor = cor;
+}
